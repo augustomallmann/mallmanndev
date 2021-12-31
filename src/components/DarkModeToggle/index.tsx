57be677830eac3ede095d4ab11924e7c4e5d731e
augustocmallmann <augustocmallmann@gmail.com>
2021-12-31 19:07:57 -0300

    feat: dark mode toggle component

---
+++ src/components/DarkModeToggle/index.tsx
@@ -0,0 +1,27 @@
+import {
+    useColorModeValue,
+    IconButton,
+    useColorMode,
+} from '@chakra-ui/react';
+import { MdDarkMode, MdWbSunny } from 'react-icons/md';
+
+export const DarkModeToggle = () => {
+
+    const { colorMode, toggleColorMode } = useColorMode()
+
+    return (
+        <IconButton
+            onClick={toggleColorMode}
+            bg={useColorModeValue('black.200', 'gray.200')}
+            color={useColorModeValue('gray.100', 'black.200')}
+            fontSize='20px'
+            aria-label="Dark Mode Toggle"
+            icon={colorMode === 'light' ? <MdDarkMode /> : <MdWbSunny />}
+            isRound={true}
+            _hover={{
+                textDecoration: 'none',
+                bg: useColorModeValue('blue.900', 'yellow.400'),
+            }}
+        />
+    )
+}
